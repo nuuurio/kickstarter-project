8396c30356482e0d37fc3a88d0632e3ad55c682f
Núria Balaguer <nyxscream@gmail.com>
2019-01-08 11:09:26 +0100

    add proxy contract and proxy tests

---
+++ contracts/Proxy.sol
@@ -0,0 +1,25 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+contract Proxy {
+    address public campaignAddress;
+    address public manager;
+
+    modifier restrictedToManager() {
+        require(manager == msg.sender, "Permission denied");
+
+        _;
+    }
+
+    constructor(address _address) public {
+        campaignAddress = _address;
+        manager = msg.sender;
+    }
+
+    function getProxyAddress() public view returns(address){
+        return campaignAddress;
+    }
+
+    function setProxyAddress(address _newAddress) public restrictedToManager {
+        campaignAddress = _newAddress;
+    }
+}
